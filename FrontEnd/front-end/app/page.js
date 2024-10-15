01487470e998dc669a7150496d199d3b8ea5fa10
Ismail-Jobi <niloy9195@gmail.com>
2024-10-15 16:09:22 +0600

    Fix nested repository issue in front-end folder

---
+++ FrontEnd/front-end/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Service from "./components/service";
+
+export default function Home() {
+  return (
+    <>
+      <div className="mockup-window bg-base-300">
+
+        <div className="bg-base-200 flex justify-center px-80 "> <Service /></div>
+
+      </div>
+
+    </>
+  );
+}
