41450255d3f81e57bb4b2363e3b3cf48f4e2a323
Ismail-Jobi <niloy9195@gmail.com>
2024-10-16 15:14:29 +0600

    React Project Added

--- FrontEnd/front-end/app/page.js
+++ FrontEnd/front-end/app/page.js
@@ -5,10 +5,11 @@ export default function Home() {
   return (
     <>
       <div className="mockup-window bg-base-300">
-
-        <div className="bg-base-200 flex justify-center px-80 "> <Service /></div>
-
+        <div className="bg-base-200 flex justify-center px-4 sm:px-8 md:px-20 lg:px-40 xl:px-80">
+          <Service />
+        </div>
       </div>
+
 
     </>
   );
--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -8,11 +8,7 @@ import Service from './components/service';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="mockup-window bg-base-300">
-
-      <div className="bg-base-200 flex justify-center px-80 "> <Service /></div>
-
-    </div>
+    <div className="bg-base-200 flex justify-center px-80 "> <Service /></div>
   </React.StrictMode>
 );
 
