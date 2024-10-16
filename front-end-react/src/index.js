64068c45830e71fa3f73fbb46fed6d6107fba236
Ismail-Jobi <niloy9195@gmail.com>
2024-10-16 12:10:51 +0600

    Build

---
+++ front-end-react/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import service from './components/service';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Service/>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
