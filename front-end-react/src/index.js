cc2a3bc0d0bce35ed6f1017587771a102283dd90
Ismail-Jobi <niloy9195@gmail.com>
2024-10-17 20:47:29 +0600

    feedbacks completed

--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -8,7 +8,7 @@ import Service from './components/service';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="bg-base-100 flex justify-center px-20 py-5 h-screen">
+    <div className="bg-base-100 flex justify-center px-4 sm:px-2 py-5 min-h-screen">
       <Service />
     </div>
   </React.StrictMode>
