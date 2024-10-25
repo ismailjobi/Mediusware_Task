190ccfc7cd5ea8cc9d0f16f4bde77e61950d6c0d
Ismail-Jobi <niloy9195@gmail.com>
2024-10-25 15:37:42 +0600

    Updated Code Added

--- front-end-react/src/components/subscribe.jsx
+++ front-end-react/src/components/subscribe.jsx
@@ -3,25 +3,29 @@ import React from "react";
 function Subscribe() {
     return (
         <div className="Subscribe">
-            <div className="bg-[#072B4A] text-white p-6 rounded-md max-w-6xl mt-10 mx-auto py-12 sm:px-6 lg:px-8 flex justify-between items-center">
-                <div>
-                    <h2 className="text-lg font-semibold">Want to know what we are up to?</h2>
-                    <p className="text-sm">Subscribe to our newsletter.</p>
+            <div className="bg-[#072B4A] text-white p-6 rounded-md max-w-7xl mt-10 mx-auto py-12 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+                    <div className="order-1 lg:order-1 space-y-2">
+                        <h2 className="text-lg font-semibold">Want to know what we are up to?</h2>
+                        <p className="text-sm">Subscribe to our newsletter.</p>
+                    </div>
+                    <form className="order-2 lg:order-2 flex flex-row sm:flex-row space-y-3 sm:space-y-0 sm:space-x-3">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            className="p-3 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-blue-500 w-full sm:w-auto"
+                        />
+                        <button
+                            type="submit"
+                            className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition w-full sm:w-auto"
+                        >
+                            Subscribe
+                        </button>
+                    </form>
                 </div>
-                <form  className="flex items-center space-x-3">
-                    <input
-                        type="email"
-                        placeholder="Enter your email"
-                        className="p-2 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                    <button
-                        type="submit"
-                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
-                    >
-                        Subscribe
-                    </button>
-                </form>
             </div>
+
+
         </div>
     );
 }
