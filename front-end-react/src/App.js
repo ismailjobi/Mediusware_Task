2a920f8482d232ad7442f1f5fd7ff40918c93104
Ismail-Jobi <niloy9195@gmail.com>
2024-10-28 11:07:28 +0600

    Responsive Code Added

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -28,45 +28,43 @@ function App() {
 
       <div className="relative z-10">
         <NavBar />
-        {/* <TabNavBar/> */}
-        {/* <MobileNavBar/> */}
         <HomeCard />
       </div>
-      <div className="mt-40 place-items-end pr-10">
+      <div className="hidden ml-[1790px] lg:flex bottom-10 right-10 mt-40 items-end">
         <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M21.5 10C21.5 9.17156 20.8284 8.5 20 8.5C19.1716 8.5 18.5 9.17156 18.5 10V16C18.5 16.8284 19.1716 17.5 20 17.5C20.8284 17.5 21.5 16.8284 21.5 16V10Z" fill="#0C98EB" />
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M4 16C4 7.16344 11.1634 0 20 0C28.8366 0 36 7.16344 36 16V24C36 32.8366 28.8366 40 20 40C11.1634 40 4 32.8366 4 24V16ZM33 16V24C33 31.1798 27.1798 37 20 37C12.8203 37 7 31.1798 7 24V16C7 8.82031 12.8203 3 20 3C27.1798 3 33 8.82031 33 16Z" fill="#0C98EB" />
+          <path fillRule="evenodd" clipRule="evenodd" d="M4 16C4 7.16344 11.1634 0 20 0C28.8366 0 36 7.16344 36 16V24C36 32.8366 28.8366 40 20 40C11.1634 40 4 32.8366 4 24V16ZM33 16V24C33 31.1798 27.1798 37 20 37C12.8203 37 7 31.1798 7 24V16C7 8.82031 12.8203 3 20 3C27.1798 3 33 8.82031 33 16Z" fill="#0C98EB" />
         </svg>
       </div>
     </div>
     <div>
-       <Service/>
+      <Service />
     </div>
     <div>
-      <AboutSection/>
+      <AboutSection />
     </div>
     <div>
-      <Worksection/>
+      <Worksection />
     </div>
     <div>
-      <Testimonials/>
+      <Testimonials />
     </div>
     <div>
-      <Projects/>
+      <Projects />
     </div>
     <div>
-      <Achievements/>
+      <Achievements />
     </div>
     <div>
-      <Subscribe/>
+      <Subscribe />
     </div>
     <div>
-      <Contact/>
+      <Contact />
     </div>
     <div>
-      <Footer/>
+      <Footer />
     </div>
-    </>
+  </>
   );
 }
 
--- front-end-react/src/components/navbar.jsx
+++ front-end-react/src/components/navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className="NavBar">
-            <header className="w-full py-5 bg-transparent">
+        <div className="MobileNavBar">
+            <header className="w-full py-4 bg-transparent">
                 <nav className="container mx-auto flex justify-between items-center px-4">
                     <div className="logo">
                         <svg width="195" height="56" viewBox="0 0 195 56" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -29,32 +35,82 @@ function NavBar() {
                                 </clipPath>
                             </defs>
                         </svg>
-
                     </div>
-                    <ul className="hidden md:flex space-x-6 text-white font-semibold">
-                        <li><a href="#">Home</a></li>
-                        <li><details>
-                            <summary>Service</summary>
-                            <ul className="bg-transparent rounded-t-none p-2">
-                                <li><a href="#">Softwate Build</a></li>
-                                <li><a href="#">Web Application</a></li>
-                            </ul>
-                        </details></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
+
+                    <div className="hidden lg:flex space-x-8 text-white">
+                        <ul className="menu menu-horizontal px-1">
+                            <li><a href="#home">Home</a></li>
+                            <li>
+                                <details>
+                                    <summary>Services</summary>
+                                    <ul className="bg-base-100 text-black rounded-t-none p-2">
+                                        <li><a>Link 1</a></li>
+                                        <li><a>Link 2</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li><a href="#projects">Projects</a></li>
+                            <li><a href="#about">About Us</a></li>
+                            <li><a href="#contact">Contact Us</a></li>
+                        </ul>
+                    </div>
                     <a href="#" className="hidden md:flex items-center text-white py-2 px-6 rounded-lg hover:bg-blue-600 border-2 transition-all gap-2">
                         <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 9H1M14 1V5M6 1V5M5.8 21H14.2C15.8802 21 16.7202 21 17.362 20.673C17.9265 20.3854 18.3854 19.9265 18.673 19.362C19 18.7202 19 17.8802 19 16.2V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H5.8C4.11984 3 3.27976 3 2.63803 3.32698C2.07354 3.6146 1.6146 4.07354 1.32698 4.63803C1 5.27976 1 6.11984 1 7.8V16.2C1 17.8802 1 18.7202 1.32698 19.362C1.6146 19.9265 2.07354 20.3854 2.63803 20.673C3.27976 21 4.11984 21 5.8 21Z" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                         Get a Schedule
                     </a>
+                    <div className="menu-icon lg:hidden">
+                        <button onClick={toggleMenu} className="text-white focus:outline-none">
+                            {isMenuOpen ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="2"
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="2"
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
                 </nav>
+
+                {isMenuOpen && (
+                    <div className="lg:hidden mt-4 bg-gray-700">
+                        <ul className="flex flex-col space-y-2 text-white p-4">
+                        <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+                            <li>
+                                <details>
+                                    <summary>Parent</summary>
+                                    <ul className="bg-base-100 text-black rounded-t-none p-2">
+                                        <li><a onClick={toggleMenu}>Link 1</a></li>
+                                        <li><a onClick={toggleMenu}>Link 2</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li><a href="#projects"onClick={toggleMenu}>Projects</a></li>
+                            <li><a href="#about"onClick={toggleMenu}>About Us</a></li>
+                            <li><a href="#contact"onClick={toggleMenu}>Contact Us</a></li>
+                        </ul>
+                    </div>
+                )}
             </header>
         </div>
     );
 }
 
 export default NavBar;
-
